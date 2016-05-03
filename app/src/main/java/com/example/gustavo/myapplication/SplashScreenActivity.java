6eb9a1b4bbd48ae44729e025f05b41578a5a31a0
Gustavo <gustavosmonteiro@gmail.com>
2016-05-02 22:40:53 -0300

    primeiras telas

---
+++ app/src/main/java/com/example/gustavo/myapplication/SplashScreenActivity.java
@@ -0,0 +1,29 @@
+package com.example.gustavo.myapplication;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Timer().schedule(new TimerTask() {
+
+            @Override
+            public void run() {
+                finish();
+
+                Intent intent = new Intent();
+                intent.setClass(SplashScreenActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        }, 6000);
+    }
+
+}
