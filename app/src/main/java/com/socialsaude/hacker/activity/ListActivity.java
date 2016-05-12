cb5c9c80bc669d57a9fe9ef18a431f0704a95028
Rôney Reis <roneydecastro@gmail.com>
2016-05-12 00:33:03 -0300

    Search View and icons added

--- app/src/main/java/com/socialsaude/hacker/activity/ListActivity.java
+++ app/src/main/java/com/socialsaude/hacker/activity/ListActivity.java
@@ -1,16 +1,23 @@
 package com.socialsaude.hacker.activity;
 
+import android.app.SearchManager;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.SearchView;
 
 import com.socialsaude.hacker.adapter.ListAdapter;
 import com.socialsaude.hacker.login.R;
@@ -20,6 +27,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ListActivity extends AppCompatActivity {
+
+    private boolean mSearchOpened;
+    private String mSearchQuery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +60,14 @@ public class ListActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        handleIntent(getIntent());
+
     }
+    @Override
+    protected void onNewIntent(Intent intent) {
+        handleIntent(intent);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -58,10 +75,34 @@ public class ListActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_list, menu);
 
-        //SearchView mSearchView = (SearchView) menu.findItem(R.id.search);
-        //setupSearchView(mSearchView);
+        SearchManager searchManager =
+               (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String query) {
+                    Log.i("Script", "Digitado:" + query);
+                    return false;
+                }
+
+                @Override
+                public boolean onQueryTextChange(String newText) {
+                    return false;
+                }
+            });
+
         return true;
 
     }
+
+    private void handleIntent(Intent intent) {
+
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            //use the query to search your data somehow
+        }
+    }
+
 
 }
